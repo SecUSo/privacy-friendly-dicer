d77a9b39aa4c31301887f2e2e8ee0bdaaa70f603
Karola Marky <yonjuni@pinyto.de>
2015-08-28 12:14:16 +0200

    Dicer class added

---
+++ app/src/main/java/privacyfriendlydicer/secuso/informatik/tudarmstadt/de/privacyfriendlydicer/Dicer.java
@@ -0,0 +1,17 @@
+package privacyfriendlydicer.secuso.informatik.tudarmstadt.de.privacyfriendlydicer;
+
+/**
+ * Created by yonjuni on 5/6/15.
+ */
+public class Dicer {
+
+    public int[] rollDice(int poolSize){
+        int[] dice = new int[poolSize];
+
+        for (int i=0;i<dice.length;i++){
+            dice[i] = (int) (Math.random() * 6) + 1;
+        }
+        return dice;
+    }
+
+}
--- app/src/main/java/privacyfriendlydicer/secuso/informatik/tudarmstadt/de/privacyfriendlydicer/MainActivity.java
+++ app/src/main/java/privacyfriendlydicer/secuso/informatik/tudarmstadt/de/privacyfriendlydicer/MainActivity.java
@@ -1,10 +1,15 @@
 package privacyfriendlydicer.secuso.informatik.tudarmstadt.de.privacyfriendlydicer;
 
+import android.app.ActionBar;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +17,42 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollDiceButton = (Button) findViewById(R.id.rollButton);
+
+        final SeekBar poolSeekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        SeekBar seekBarLength = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBarLength.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView textViewLengthDisplay =
+                        (TextView) findViewById(R.id.chooseDiceNumber);
+                textViewLengthDisplay.setText(Integer.toString(progress + 1));
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        rollDiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String resultString = "";
+                Dicer dicer = new Dicer();
+                TextView result = (TextView) findViewById(R.id.textViewResults);
+                int[] dice = dicer.rollDice(poolSeekBar.getProgress() + 1);
+
+                for (int i=0;i<dice.length;i++) {
+                    resultString += dice[i];
+                }
+
+                result.setText(resultString);
+            }
+        });
     }
 
     @Override
