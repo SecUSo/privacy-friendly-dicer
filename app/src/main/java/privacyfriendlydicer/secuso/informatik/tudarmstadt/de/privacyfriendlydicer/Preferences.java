0c306486446efcdd55eebd8fcb3b01f1ade59012
Karola Marky <yonjuni@pinyto.de>
2015-09-25 19:49:15 +0200

    Parentactivity from preferences added, so that the back button can FINALLY work

--- app/src/main/java/privacyfriendlydicer/secuso/informatik/tudarmstadt/de/privacyfriendlydicer/Preferences.java
+++ app/src/main/java/privacyfriendlydicer/secuso/informatik/tudarmstadt/de/privacyfriendlydicer/Preferences.java
@@ -1,8 +1,9 @@
 package privacyfriendlydicer.secuso.informatik.tudarmstadt.de.privacyfriendlydicer;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by yonjuni on 31.08.15.
@@ -16,12 +17,32 @@ public class Preferences extends ActionBarActivity{
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(R.string.action_settings);
 
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new SettingsFragment()).commit();
 
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
