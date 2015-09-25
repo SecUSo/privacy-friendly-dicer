93340b67d0f93aeaa84aa7b8832653b2242ac7e8
Karola Marky <yonjuni@pinyto.de>
2015-09-25 19:37:34 +0200

    Actionbar in Settings but not clickable YET

--- app/src/main/java/privacyfriendlydicer/secuso/informatik/tudarmstadt/de/privacyfriendlydicer/Preferences.java
+++ app/src/main/java/privacyfriendlydicer/secuso/informatik/tudarmstadt/de/privacyfriendlydicer/Preferences.java
@@ -2,19 +2,25 @@ package privacyfriendlydicer.secuso.informatik.tudarmstadt.de.privacyfriendlydic
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by yonjuni on 31.08.15.
  */
-public class Preferences extends PreferenceActivity{
+public class Preferences extends ActionBarActivity{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new SettingsFragment()).commit();
+
+
 
     }
 
